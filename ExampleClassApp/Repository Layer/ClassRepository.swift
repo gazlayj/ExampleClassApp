a7885410fdd2718321d79c90d1069247c5dec4ba
Justin Gazlay <justin.gazlay@gmail.com>
2020-03-15 12:53:38 -0600

    add data persistence comments

--- ExampleClassApp/Repository Layer/ClassRepository.swift
+++ ExampleClassApp/Repository Layer/ClassRepository.swift
@@ -9,6 +9,16 @@
 import Foundation
 
 class ClassRepository {
+    /*
+     This is currently just loading some dummy static data but the next step would be to build out
+     a persitant store of some sort for it to load from and write to. i.e. straight to disk, coredata, sql, etc.
+     
+     It would be resposnible for knowing when to update the persitant store. If updating is fast it might do it on every change, otherwise it might only do it on the app going to background.
+     
+     The data models that are stored to the persistant store might be somewhat different from the Models this consumes and returns. For instance it in this case it would probably make more sense for the store model of a class to only include the id's of the teacher and student instead of the entire people models which could end up being persisted many times over as a single person might be in many classes.
+     
+     In that case it would be better to just save the ids of all the people associated with the class and then when reading from the persistent store it could ask the PeopleRepository (who should be in charge of managing a persistent store of all the people) for the Person models it needs based on their ids.
+     */
     private var allClasses = Set(SchoolClass.defaultClasses)
     
     init() { }
